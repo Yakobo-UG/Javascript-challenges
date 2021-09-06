8a8b7e88a3d83dc2c085ed3f6097b49bcea12405
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-06 20:28:59 +0300

    challenge 2
    Ask for the user’s first name and then ask for
    their surname and display the output message

---
+++ challenge 2.js
@@ -0,0 +1,6 @@
+/* Ask for the user’s first name and then ask for 
+their surname and display the output message */
+
+var firstname = prompt("Enter first name: ");
+var surname = prompt("Enter surname: ");
+console.log(firstname, surname);
