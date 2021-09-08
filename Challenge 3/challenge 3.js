6e06d0c009f6d76f9beeab919a1e3203a5cae6b4
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-08 23:33:10 +0300

    challenge 3
    Write code that will display the joke “What do you call a bear with no
    teeth?” and on the next line display the answer “A gummy bear!” Try to
    create it using only one line of code.

---
+++ Challenge 3/challenge 3.js
@@ -0,0 +1,4 @@
+/*Write code that will display the joke “What do you call a bear with no 
+teeth?” and on the next line display the answer “A gummy bear!” Try to 
+create it using only one line of code. */
+console.log( "What dp you call a bear with no teeth? \n A gummy bear")
