a4077061c3c26b2650cca3bf44bacc07bd3bbe03
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-08 23:33:42 +0300

    challenge 4
    Ask the user to enter two numbers. Add them together and displaythe answer as The total is [answer].

---
+++ challenge 4/challenge 4.js
@@ -0,0 +1,5 @@
+/*Ask the user to enter two numbers. Add them together and displaythe answer as The total is [answer].*/
+var num1 = parseInt(prompt("Enter first number here: "), 10);
+var num2 = parseInt(prompt("Enter second number here: "), 10);
+var Total = num1 + num2;
+console.log(Total);
