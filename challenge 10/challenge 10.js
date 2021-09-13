9b701f3c0f0ea1931e6a7447cdc0dfc05de16b01
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-13 09:22:53 +0300

    challenge 10
    there are 2,204 pounds in a kilogram. Ask the user to enter a weight in kilograms and convert it to pounds

---
+++ challenge 10/challenge 10.js
@@ -0,0 +1,5 @@
+/*There are 2,204 pounds in a kilogram. Ask the user to enter a weight in kilograms and convert it to pounds. */
+/* 1kg --> 2204 ppounds*/
+weight = parseInt(prompt("Enter weight in kilograms: "));
+convert = weight * 2204;
+console.log("There are ", convert, "pounds in", weight, "kilograms");
