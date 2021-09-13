ac15f7ee166e4f367869dcf23e1230f017009a31
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-13 09:24:05 +0300

    challenge 7
    *Ask the user for their name and their age. Add 1 to their age
    and display the output [Name] next birthday you
    will be [new age]

---
+++ challenge 7/challenge 7.js
@@ -0,0 +1,8 @@
+/*Ask the user for their name and their age. Add 1 to their age 
+and display the output [Name] next birthday you 
+will be [new age].*/
+
+var nam = prompt("Enter your name: ");
+var age = parseInt(prompt("Enter your age: "));
+var add = age + 1;
+console.log(nam, "next birthday you will be", add);
