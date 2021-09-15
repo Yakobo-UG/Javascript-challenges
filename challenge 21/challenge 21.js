5d4e080f8c334528758d692aeff935be818c8339
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-16 00:29:27 +0300

    challenge 21
    Ask the user to enter their first name and then ask them to enter their surname. Join them together with a space between and display the name and the length of whole name.

---
+++ challenge 21/challenge 21.js
@@ -0,0 +1,7 @@
+//Ask the user to enter their first name and then ask them to enter their surname. Join them together with a space between and display the name and the length of whole name.
+var firstname = prompt("Enter first name: ");
+    surname = prompt("Enter surname name: ");
+    join = firstname +" "+ surname;
+    len = join.length;
+console.log(join);
+console.log(len);
