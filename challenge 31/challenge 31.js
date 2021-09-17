fe0813df9d3f353bf2440dc47068d4490f952c42
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-17 18:14:19 +0300

    challenge 31
    Ask the user to enter the radius of a circle (measurement from the centre point to the edge). Work out the area of the circle (π*radius2).

---
+++ challenge 31/challenge 31.js
@@ -0,0 +1,6 @@
+//Ask the user to enter the radius of a circle (measurement from the centre point to the edge). Work out the area of the circle (π*radius2). 
+
+var radius = parseFloat(prompt("Enter radius of a circle: "));
+    pi = Math.PI;
+    rad = Math.pow(radius,2);
+console.log(pi*rad);
