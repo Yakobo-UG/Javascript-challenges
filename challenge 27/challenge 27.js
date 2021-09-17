5ecc3ac9948fbc8c45ebf19c5a9dc8181b043471
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-17 18:12:57 +0300

    challenge 27
    Ask the user to enter a number with lots of decimal places. Multiply this number by two and display the answer.

---
+++ challenge 27/challenge 27.js
@@ -0,0 +1,5 @@
+//Ask the user to enter a number with lots of decimal places. Multiply this number by two and display the answer.
+
+var num = parseFloat(prompt("Enter number with very many decimals: "));
+    mult = num * 2;
+console.log(mult);
