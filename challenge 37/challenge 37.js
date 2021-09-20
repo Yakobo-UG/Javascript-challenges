acae35c6ca43d74e2a04d623f9fa978e22facb9c
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-20 22:55:17 +0300

    challenge 37
    Ask the user to enter their name and display each letter in their name on a separate line.

---
+++ challenge 37/challenge 37.js
@@ -0,0 +1,6 @@
+//Ask the user to enter their name and display each letter in their name on a separate line.
+var UserName = prompt("Enter your name: ");
+for (var i = 0; i < UserName.length; i++){
+    console.log(UserName[i]);
+
+}
