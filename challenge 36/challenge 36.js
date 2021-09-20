5a91595d9be8e5d06a5eb73201600f17165d19b0
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-20 22:55:00 +0300

    challenge 36
    Alter program 035 so that it will ask the user to enter their name and a number and then display their name that number of times.

---
+++ challenge 36/challenge 36.js
@@ -0,0 +1,7 @@
+//Alter program 035 so that it will ask the user to enter their name and a number and then display their name that number of times. 
+
+var nam = prompt("Enter your name: ");
+    num = parseFloat(prompt("Enter a number"));
+for (var i = 0; i < num; i++ ){
+    console.log(nam);
+}
