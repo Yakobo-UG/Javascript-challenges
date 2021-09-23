2b5d83f30a26ad06193e885f21a44a4591dbbc9c
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-23 17:04:36 +0300

    challenge 3
    Show the conversion from number to string and vice versa
    //- Conversion between numbers and strings is a common practical use case

---
+++ Additional challenges/Primitive concepts/challenge 3/challenge 3.js
@@ -0,0 +1,18 @@
+//Show the conversion from number to string and vice versa
+//- Conversion between numbers and strings is a common practical use case
+
+//number to string
+var num = 1;
+num.toString();
+console.log(typeof num);
+
+//string to numner
+var str = "james";
+parseInt(str);
+console.log(typeof str);
+
+// float number to integer
+var int = 344;
+parseFloat(int);
+console.log(int);
+
