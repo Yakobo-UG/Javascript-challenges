802e7f930c3fc3cdef57a1b569929f3db57db4ab
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-23 17:04:20 +0300

    challenge 2
    Show the different ways of concatenating numbers and strings
    //- Concatenation of strings and numbers is a common practical use case

---
+++ Additional challenges/Primitive concepts/challenge 2/challenge 2.js
@@ -0,0 +1,8 @@
+//Show the different ways of concatenating numbers and strings
+//- Concatenation of strings and numbers is a common practical use case
+console.log("James" + 1);
+console.log("James" + "Yakobo");
+console.log("James" + (1 + 10));
+console.log("1" + 1);
+console.log("123" + 1);
+
