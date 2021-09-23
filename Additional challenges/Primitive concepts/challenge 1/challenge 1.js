b98d3de28cc70d6691656bfed5c0f287aa62c383
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-23 17:04:02 +0300

    challenge 1
    how the usage of `typeof` operator on different types of values
    //- The `typeof` operator returns a string indicating the type of the operand

---
+++ Additional challenges/Primitive concepts/challenge 1/challenge 1.js
@@ -0,0 +1,23 @@
+//how the usage of `typeof` operator on different types of values
+//- The `typeof` operator returns a string indicating the type of the operand
+console.log(typeof 5);
+console.log(typeof "James");
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof 5.7);
+console.log(typeof NaN);
+console.log(typeof undefined);
+console.log(typeof Infinity);
+console.log(typeof null);
+console.log(typeof function(){});
+console.log(typeof {});
+console.log(typeof [2,4]);
+console.log(typeof 3n);
+console.log(typeof Symbol());
+
+
+
+
+
+
+
