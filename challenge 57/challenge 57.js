6207a8308c1e0223edf014eb05b529b80e56eaf1
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-24 15:07:53 +0300

    challenge 57
    Enter a list of ten colours. Ask the user for a starting number between 0 and 4 and an end number between 5 and 9. Display the list for those colours between the start and end numbers the user input.

---
+++ challenge 57/challenge 57.js
@@ -0,0 +1,9 @@
+//Enter a list of ten colours. Ask the user for a starting number between 0 and 4 and an end number between 5 and 9. Display the list for those colours between the start and end numbers the user input.
+
+var list = ["Yellow", "red", "pink", "blue", "orange", "cyan", "black", "violet", "marron", "magenta"];
+    start = parseInt(prompt("Enter a starting number: "));
+    end = parseInt(prompt("Enter an end number: "));
+    cut = list.slice(start, end);
+console.log(cut)
+
+
