8a8a2b7ca3adda2bd317d92845ab24d693cca3a8
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-10-01 17:06:27 +0300

    challenge 1
    swap 2 integers present in variables num1 and num2 without using temporary variable

---
+++ Additional challenges/Primitive challenges/challenge 1/challenge 1.js
@@ -0,0 +1,13 @@
+//Swap 2 integers present in variables num1 and num2 without using temporary variable
+var num1 = 1;
+    num2 = 2;
+    num3 = 3;
+    num4 = 4;
+
+changede = [num1, num2] = [num2, num1];
+changede2 = [num3, num4] = [num4, num3]
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
