64064b7039f3d634b7b74a54147cada921194326
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-10-01 17:08:15 +0300

    challenge 6
    /Write a code to replace all the spaces of the string with underscores

---
+++ Additional challenges/Primitive challenges/challenge 6/challenge 6.js
@@ -0,0 +1,6 @@
+//Write a code to replace all the spaces of the string with underscores
+var word = "james muhumuza Kamulindwa hahaha wjdekbf f  jbe jfb f ibe ib ";
+word.split(" ").join("_");
+console.log(word);
+
+// not sure why its not working
