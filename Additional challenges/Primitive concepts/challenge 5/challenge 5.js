093e57a77a3d20ca14fb526542a3a12d3eda72b3
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-10-01 17:09:16 +0300

    challenge 5
    different method of displaying each character on a different line in javascritp

---
+++ Additional challenges/Primitive concepts/challenge 5/challenge 5.js
@@ -0,0 +1,15 @@
+//
+var str = "James";
+for( let i =0; i < str.length; i++){
+    console.log(str.charAt(i));
+}
+
+for( let index in str){
+    console.log(index);
+}
+
+for(let value of str){
+    console.log(value);
+}
+
+    
