e2c611a320b0d22f2bd2b50b85cf2928d59f16ba
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-10-02 14:12:06 +0300

    Update challenge 38.js

--- challenge 38 fail/challenge 38.js
+++ challenge 38 fail/challenge 38.js
@@ -1,10 +1,11 @@
 //Change program 037 to also ask for a number. Display their name (one letter at a time on each line) and repeat this for the number of times they entered.
 
 var UserName = prompt("Enter your name: ");
-    UserNum = parseFloat(prompt("Enter number: "));
+    UserNum = prompt("Enter number: ");
 for (var i = 0; i < UserNum; i++){
-    console.log(UserName[i]);
-
+    for (var x = 0; i in UserName; i++){
+        console.log(UserName.charAt(x));
+    }
 }
 
 //completely failed this one any help 
