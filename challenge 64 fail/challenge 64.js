3e70170e016dcf7e7cbffb8996bb0bc25a09ec23
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-10-02 14:16:45 +0300

    challenge 64
    Ask the user to type in their favourite school subject. Display it with “-” after each letter, e.g. S-p-a-n-i-s-h-

---
+++ challenge 64 fail/challenge 64.js
@@ -0,0 +1,4 @@
+//Ask the user to type in their favourite school subject. Display it with “-” after each letter, e.g. S-p-a-n-i-s-h-
+var best = prompt("ENter your best subject: ");
+best.substr(0, "-", best.length);
+//failed
