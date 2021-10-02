4f7465e2d6c49e73bfdf1fc5302a0072584acb5d
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-10-02 14:17:10 +0300

    challeneg 65
    how the user a line of text from your favourite poem and ask for a starting and ending point. Display the characters between those two points.

---
+++ challenge 65/challenge 65.js
@@ -0,0 +1,8 @@
+//Show the user a line of text from your favourite poem and ask for a starting and ending point. Display the characters between those two points. 
+var line = "thy yo  man this is going great man yo yoyoy y haha very funny hhahahah very reallty hacker true"
+console.log(line);
+var start = parseInt(prompt("Enter a starting point"));
+    end = parseInt(prompt("ENter end point: "));
+line.split()
+var got = line.slice(start, end);
+console.log(got);
