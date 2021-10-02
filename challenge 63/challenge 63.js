9621513e05621663afdba35f636d5df8f0df7bc7
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-10-02 14:12:31 +0300

    challenge 63
    Ask the user to enter their first name and then display the length of their first name. Then ask for their surname and display the length of their surname. Join their first name and surname together with a space between and display the result. Finally, display the length of their full name (including the space)

---
+++ challenge 63/challenge 63.js
@@ -0,0 +1,8 @@
+/*Ask the user to enter their first name and then display the length of their first name. Then ask for their surname and display the length of their surname. Join their first name and surname together with a space between and display the result. Finally, display the length of their full name (including the space)*/
+var firstName = prompt("Enter your first name: ");
+console.log(firstName.length);
+var Surname = prompt("Enetr your surname: ");
+console.log(Surname.length);
+console.log(firstName + " " + Surname);
+var full = firstName + " " + Surname;
+console.log(full.length);
